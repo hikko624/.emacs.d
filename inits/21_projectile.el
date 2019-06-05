4b6c0556ce7dca1d6bb2ba54dc08b893c2682e96
HikosakaTakumi <hikko624@H-2007.local>
2019-06-05 18:03:08 +0900

    add helm projectile settings

--- inits/21_projectile.el
+++ inits/21_projectile.el
@@ -1,7 +1,5 @@
-;; (use-package projectile
-;;   :ensure t
-;;   :init
-;;   (projectile-global-mode))
+(use-package projectile
+  :ensure t)
 
 (use-package projectile-rails
   :ensure t
@@ -10,8 +8,24 @@
 
 (use-package helm-projectile
   :ensure t
+  :init
+  (setq projectile-enable-caching t)
   :bind (
+         ;; 参考 http://tuhdo.github.io/helm-projectile.html
          ("C-c p h" . helm-projectile)
+         ("C-c p p" . helm-projectile-switch-project)
+         ("C-c p f" . helm-projectile-find-file)
+         ("C-c p F" . helm-projectile-find-file-in-known-projects)
+         ("C-c p g" . helm-projectile-find-file-dwim)
+         ("C-c p d" . helm-projectile-find-dir)
+         ("C-c p e" . helm-projectile-recentf)
+         ("C-c p a" . helm-projectile-find-other-file)
+         ("C-c p i" . projectile-invalidate-cache)
+         ("C-c p z" . projectile-cache-current-file)
+         ("C-c p b" . helm-projectile-switch-to-buffer)
+         ("C-c p s g" . helm-projectile-grep)
+         ("C-c p s a" . helm-projectile-ack)
+         ("C-c p s s" . helm-projectile-ag)
          )
   :config
   (helm-projectile-on)
