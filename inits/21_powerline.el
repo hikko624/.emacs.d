f06d16f0a4b301ef08111861a8f1eaf72a0b2c8c
hikko624 <hikko624@gmail.com>
2019-08-28 13:52:23 +0900

    update powerline color

--- inits/21_powerline.el
+++ inits/21_powerline.el
@@ -1,5 +1,47 @@
 (use-package powerline
   :ensure t
+  :config
+  (defconst color1 "#05003d")
+  (defconst color2 "#504f66")
+  (set-face-attribute 'mode-line nil
+                      :foreground "#fff"
+                      :background color1
+                      :bold t
+                      :box nil)
+
+  (set-face-attribute 'powerline-active1 nil
+                      :foreground "#fff"
+                      :background color2
+                      :bold t
+                      :box nil
+                      :inherit 'mode-line)
+
+  (set-face-attribute 'powerline-active2 nil
+                      :foreground "white smoke"
+                      :background "gray20"
+                      :bold t
+                      :box nil
+                      :inherit 'mode-line)
+
+  (set-face-attribute 'mode-line-inactive nil
+                      :foreground "#fff"
+                      :background color1
+                      :bold t
+                      :box nil)
+
+  (set-face-attribute 'powerline-inactive1 nil
+                      :foreground "#fff"
+                      :background color2
+                      :bold t
+                      :box nil
+                      :inherit 'mode-line)
+
+  (set-face-attribute 'powerline-inactive2 nil
+                      :foreground "white smoke"
+                      :background "gray20"
+                      :bold t
+                      :box nil
+                      :inherit 'mode-line)
   :init
   (powerline-default-theme)
   )
