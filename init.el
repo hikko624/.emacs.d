d2829b2cb61b56b3586d6019c0b5ae814e88999d
hikko624 <hikko624@gmail.com>
2019-11-06 00:45:56 +0900

    update inits file

--- init.el
+++ init.el
@@ -23,6 +23,16 @@
   (package-install 'use-package))
 (require 'use-package)
 ;; init-loader
+(setq debug-on-error t)
+(use-package exec-path-from-shell
+  :ensure t
+  :if (memq window-system '(mac ns x))
+  :config
+  (exec-path-from-shell-initialize)
+  ;; (exec-path-from-shell-copy-env "GOPATH")
+  (exec-path-from-shell-copy-env "SDKROOT")
+  )
+
 (use-package init-loader
   :ensure t
   :config
--- inits/21_exec-path-from-shell.el
+++
@@ -1,8 +0,0 @@
-(use-package exec-path-from-shell
-  :ensure t
-  :if (memq window-system '(mac ns x))
-  :config
-  (exec-path-from-shell-initialize)
-  ;; (exec-path-from-shell-copy-env "GOPATH")
-  (exec-path-from-shell-copy-env "SDKROOT")
-  )
