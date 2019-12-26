4826d67750f5f7d220aed6875f817811b78203a0
hikko624 <hikko624@gmail.com>
2019-12-27 00:06:40 +0900

    fix magit setting

--- inits/10_dired-subtree.el
+++ inits/10_dired-subtree.el
@@ -5,5 +5,6 @@
          ;; iを置き換え(展開する)
          ("i" . dired-subtree-insert)
          ;; org-modeのようにTABで折り畳む(magitのようにtabで展開もできるようにする)
-         ;; ("<tab>" . dired-subtree-remove)
-         ("<tab>" . dired-subtree-toggle)))
+         ("<tab>" . dired-subtree-remove)
+         ;; ("<tab>" . dired-subtree-toggle)
+         ))
