5d432900df52dc37d7722ada5a32fab31613ebe6
hikko624 <hikko624@gmail.com>
2020-10-19 02:39:37 +0900

    fix emacs setting

--- inits/20_golden-ratio.el
+++ inits/20_golden-ratio.el
@@ -6,10 +6,10 @@
   ;; ウィンドウの大きさを変更しないメジャーモード
   (setq golden-ratio-exclude-modes '(calendar-mode))
   ;;; ウィンドウ選択系のコマンドで作用させる
-  (setq golden-ratio-extra-commands
-        '(windmove-left windmove-right windmove-down windmove-up))
+  ;; (setq golden-ratio-extra-commands
+  ;;       '(windmove-left windmove-right windmove-down windmove-up))
   ;; ウィンドウの大きさを変更しないバッファ名
-  (setq golden-ratio-exclude-buffer-names '(" *Org tags*" " *Org todo*"))
+  ;; (setq golden-ratio-exclude-buffer-names '(" *Org tags*" " *Org todo*"))
   ;; ウィンドウの大きさを変更しないバッファ名の正規表現
-  (setq golden-ratio-exclude-buffer-regexp '("\\*helm" "magit*\\"))
+  ;; (setq golden-ratio-exclude-buffer-regexp '("\\*helm"))
   )
