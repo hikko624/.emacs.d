264c9ab0eadf8e5ff5981cdc772163e1b849c2ba
hikko624 <hikko624@gmail.com>
2020-10-25 01:48:39 +0900

    delete posframe settings

--- inits/30_company-posframe.el
+++
@@ -1,4 +0,0 @@
-(use-package company-posframe
-  :straight t
-  :config
-  (company-posframe-mode 1))
--- inits/30_hydra-posframe.el
+++
@@ -1,4 +0,0 @@
-(use-package hydra-posframe
-  :straight (:host github :repo "Ladicle/hydra-posframe" :branch "master")
-  :hook
-  (after-init . hydra-posframe-enable))
