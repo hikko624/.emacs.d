cc7ffc0355e434335050d9348a868f27ba029d71
hikko624 <hikko624@gmail.com>
2022-06-02 15:59:27 +0900

    update inits

--- inits/10_undo-tree.el
+++ inits/10_undo-tree.el
@@ -3,5 +3,8 @@
   :diminish
   :init
   (global-undo-tree-mode)
-  :custom ((undo-tree-auto-save-history t)
-           (undo-tree-history-directory-alist '(("." . "~/.emacs.d/undo-tree")))))
+  :custom (
+           ;; (undo-tree-auto-save-history t)
+           ;; (undo-tree-load-history t)
+          (undo-tree-history-directory-alist '(("." . "~/.emacs.d/undo-tree"))))
+)
